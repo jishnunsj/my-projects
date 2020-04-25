7ba81898eb36c2b9d297eebdb2735c878549bb8a
jishnunsj <jishnunsj@gmail.com>
2020-04-25 09:30:03 +0530

    button added

--- project_1.py
+++ project_1.py
@@ -3,22 +3,33 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+
 
 class MyGrid(GridLayout):
     def __init__(self, **kwargs):
         super(MyGrid, self).__init__(**kwargs)
-        self.cols = 2
-        self.add_widget(Label(text="Name:"))
-        self.name = TextInput(multiline=False)
-        self.add_widget(self.name)
+        self.cols = 1
 
-        self.add_widget(Label(text="Name:"))
+        self.inside = GridLayout()
+        self.inside.cols = 2
+
+        self.inside.add_widget(Label(text="First Name:"))
         self.name = TextInput(multiline=False)
-        self.add_widget(self.name)
+        self.inside.add_widget(self.name)
+
+        self.inside.add_widget(Label(text="Last Name:"))
+        self.name = TextInput(multiline=False)
+        self.inside.add_widget(self.name)
         
-        self.add_widget(Label(text="Name:"))
+        self.inside.add_widget(Label(text="Email:"))
         self.name = TextInput(multiline=False)
-        self.add_widget(self.name)
+        self.inside.add_widget(self.name)
+
+        self.add_widget(self.inside)
+
+        self.submit = Button(text="Submit",font_size=40)
+        self.add_widget(self.submit)
 class myapp(App):
     def build(self):
         return MyGrid()
